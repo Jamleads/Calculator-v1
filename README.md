8a3c944451953beced806d569c756a194a72b717
AdetunjiJakes <68664742+Jamleads@users.noreply.github.com>
2023-05-16 14:06:46 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Title: Simple Logic Calculator
+
+Simple Logic calculator is made of HTML, CSS and JAVASCRIPT: I made it as a practice of my learning as a Javascrit beginer.
+The calculator is expected to perform simple calculation function only for now.
+Advance logo calculation function will be add with time
+
+## Features
+
+- Addition: The process of calculating the total of two or more numbers
+- Subtraction: The process of deducting number away from another number to obtain the remaining diffrence
+- devision: The action of seperating numbers into part
+- Multiplication: The product of two or more numbers
+- clear: Clear the screen
+- Delete: removing the numbers on the screen singularly
+
+## Usage
+
+This can only be use for basic calculation of numbers... anywhere, anything.
+
+## Contributing
+
+Anyone can contribute to this project, add to the html file or update the css design for better responsiveness and more advance functionalities in javascript. I will also be upating the calculator to make sure I finally acheive a sientivic an advance calculator.
+
+## Contact
+
+ogunyemiadetunji17@gmail.come
---
+++ script.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelector('.cal-keys');
+const display = document.querySelector('.cal-display');
+
+keys.addEventListener('click', function (e) {
+  if (e.target.matches('button')) {
+    const key = e.target;
+    const action = key.dataset.action;
+
+    if (!action) {
+      console.log('number key');
+    }
+
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      console.log('operator key');
+    }
+
+    // listening to other keys singularly
+    if (action === 'decimal') {
+      console.log('decimal key');
+    }
+
+    if (action === 'clear') {
+      console.log('clear key');
+    }
+
+    if (action === 'calculate') {
+      console.log('equaly key');
+    }
+
+    if (action === 'delete') {
+      console.log('delete key');
+    }
+
+    if (action === 'percentage') {
+      console.log('percentage key');
+    }
+  }
+});
+
+keys.addEventListener('click', function (e) {
+  if (e.target.matches('button')) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+
+    if (!action) {
+      if (displayedNum === '0') {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+
+    if (action === 'decimal') {
+      if (actionAfterOperator()) {
+        display.textContent = '0.';
+      } else if (!displayedNum.includes('.')) {
+        display.textContent = displayedNum + '.';
+      }
+    }
+
+    function actionAfterOperator() {
+      const operatorKeys = document.querySelectorAll(
+        '[data-action="divide"], [data-action="multiply"], [data-action="subtract"], [data-action="add"] '
+      );
+
+      return (
+        Array.from(operatorKeys).some(key =>
+          key.classList.contains('is-pressed')
+        ) && calculator.dataset.operator
+      );
+    }
+
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      key.classList.add('is-pressed');
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+
+    if (action === 'clear') {
+      display.textContent = 0;
+    }
+
+    const operatorKeys = document.querySelectorAll(
+      '[data-action="divide"], [data-action="multiply"], [data-action="subtract"], [data-action="add"] '
+    );
+    const operatorPressed = Array.from(operatorKeys).some(key =>
+      key.classList.contains('is-pressed')
+    );
+    if (operatorPressed && !action) {
+      display.textContent = keyContent;
+      operatorKeys.forEach(key => key.classList.remove('is-pressed'));
+    }
+
+    if (action === 'calculate') {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+      console.log(firstValue);
+      console.log(operator);
+      console.log(secondValue);
+
+      const calculate = function (n1, operator, n2) {
+        let result = '';
+        if (operator === 'add') {
+          result = parseFloat(n1) + parseFloat(n2);
+        } else if (operator === 'subtract') {
+          result = parseFloat(n1) - parseFloat(n2);
+        } else if (operator === 'multiply') {
+          result = parseFloat(n1) * parseFloat(n2);
+        } else if (operator === 'divide') {
+          result = parseFloat(n1) / parseFloat(n2);
+        }
+        return result;
+      };
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+    }
+
+    if (action === 'delete') {
+      const displayedNumArray = Array.from(displayedNum);
+      displayedNumArray.pop();
+      display.textContent = displayedNumArray.join('');
+      console.log(displayedNumArray);
+    }
+  }
+});
